53689e6c27e3ba7bebf8dfd9900ad5bdff1bd4db
mwkim <9sjymnk@naver.com>
2025-09-15 23:26:02 +0900

    수정

--- plugins/hooks/custom_postgres_hook.py
+++ plugins/hooks/custom_postgres_hook.py
@@ -15,33 +15,44 @@ class CustomPostgresHook(BaseHook):
         self.dbname = airflow_conn.schema
         self.port = airflow_conn.port
 
-        self.postgres_conn = psycopg2.connect(host=self.host, user=self.user, password=self.password, dbname=self.dbname, port=self.port)
+        self.postgres_conn = psycopg2.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            dbname=self.dbname,
+            port=self.port
+        )
         return self.postgres_conn
 
-    def bulk_load(self, table_name, file_name, delimiter: str, is_header: bool, is_replace: bool):
+    def bulk_load(self, table_name, file_name, delimiter: str = '\t', is_header: bool = True, is_replace: bool = True):
         from sqlalchemy import create_engine
 
-        self.log.info('적재 대상파일:' + file_name)
-        self.log.info('테이블 :' + table_name)
+        self.log.info('적재 대상파일: ' + file_name)
+        self.log.info('테이블: ' + table_name)
         self.get_conn()
-        header = 0 if is_header else None                       # is_header = True면 0, False면 None
-        if_exists = 'replace' if is_replace else 'append'       # is_replace = True면 replace, False면 append
+
+        header = 0 if is_header else None                 # is_header = True면 첫 행을 컬럼으로 사용
+        if_exists = 'replace' if is_replace else 'append' # True면 덮어쓰기, False면 append
+
         file_df = pd.read_csv(file_name, header=header, delimiter=delimiter)
 
-        for col in file_df.columns:                             
+        for col in file_df.columns:
             try:
-                # string 문자열이 아닐 경우 continue
-                file_df[col] = file_df[col].str.replace('\r\n','')      # 줄넘김 및 ^M 제거
+                # 문자열일 경우만 줄바꿈 제거
+                file_df[col] = file_df[col].astype(str).str.replace('\r\n', '').str.replace('\n', '')
                 self.log.info(f'{table_name}.{col}: 개행문자 제거')
-            except:
-                continue 
-                
-        self.log.info('적재 건수:' + str(len(file_df)))
-        uri = f'postgresql://{self.user}:{self.password}@{self.host}/{self.dbname}'
+            except Exception:
+                continue
+
+        self.log.info('적재 건수: ' + str(len(file_df)))
+
+        uri = f'postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.dbname}'
         engine = create_engine(uri)
-        file_df.to_sql(name=table_name,
-                            con=engine,
-                            schema='public',
-                            if_exists=if_exists,
-                            index=False
-                        )
+
+        file_df.to_sql(
+            name=table_name,
+            con=engine,
+            schema='public',
+            if_exists=if_exists,
+            index=False
+        )
